68f6faf3fcc36a0160c7e1763a596639611cb345
Kelson Eduardo <kfeduardo2003@gmail.com>
2024-08-27 19:34:14 -0300

    feat: add product model

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,26 @@
-ActiveRecord::Schema[7.2].define(version: 2024_08_27_211749) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.2].define(version: 2024_08_27_223242) do
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "store_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["store_id"], name: "index_products_on_store_id"
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -22,5 +44,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_27_211749) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "stores"
   add_foreign_key "stores", "users"
 end
